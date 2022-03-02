50ed44c2e1ca5a2b8d9f7b74c75e430b54dd61a4
Sachin Singh <33282881+idowhatido@users.noreply.github.com>
2022-03-02 13:49:23 +0530

    This is also a test :)

--- Test.java
+++ Test.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 class Test{
     public static void main(String[] args){
-        System.out.println("Hello, This is a test :)")
+        System.out.println("Hello, This is a test :)");
+        System.out.println("Okay :)");
     }
 }s
